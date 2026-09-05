1f8cc00f069d182fe293064daa313501cb752e06
MRABEH <mrabehfathi@gmail.com>
2026-09-05 12:04:20 +0200

    chore(android): initialize Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'es.mrabeh.chuletac1',
+  appName: 'Chuleta C1',
+  webDir: 'dist'
+};
+
+export default config;
